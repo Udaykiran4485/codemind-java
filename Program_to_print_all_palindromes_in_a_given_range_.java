aeba6854251e2fa294d3d18d3fd3952e9eafb890
Udaykiran4485 <112751558+Udaykiran4485@users.noreply.github.com>
2022-09-03 15:04:10 +0530

    import java.util.Scanner; class sample {
            public static boolean isPalindrome(int n)
        {
             int temp=n;
             int sum=0,r;
             while(n!=0)
               {
                  r=n%10;
                  sum=sum*10+r;
                  n=n/10;
               }
               if(temp==sum)
                     return true;
              else
                     return false;
            }
            public static void main(String args[])
            {
                Scanner sc=new Scanner(System.in);
                int a,b;
                a=sc.nextInt();
                b=sc.nextInt();
                
                int count=0;
                for(int i=a;i<=b;i++)
                {
                    if(isPalindrome(i))
                    {
                        System.out.print(i+" ");
                        
                    }
                }
               
                
                
            }
    }

---
+++ Program_to_print_all_palindromes_in_a_given_range_.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+class sample
+{
+   
+        
+        public static boolean isPalindrome(int n)
+    {
+         int temp=n;
+         int sum=0,r;
+         while(n!=0)
+           {
+              r=n%10;
+              sum=sum*10+r;
+              n=n/10;
+           }
+           if(temp==sum)
+                 return true;
+          else
+                 return false;
+        }
+        public static void main(String args[])
+        {
+            Scanner sc=new Scanner(System.in);
+            int a,b;
+            a=sc.nextInt();
+            b=sc.nextInt();
+            
+            int count=0;
+            for(int i=a;i<=b;i++)
+            {
+                if(isPalindrome(i))
+                {
+                    System.out.print(i+" ");
+                    
+                }
+            }
+           
+            
+            
+        }
+}
